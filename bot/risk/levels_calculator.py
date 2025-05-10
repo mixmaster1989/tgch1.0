4dbfe8e69f4e50bbad9c13bbb3f6f0deb9df5e83
mixmaster1989 <igoritmagic@gmail.com>
2025-05-10 16:08:08 +0300

    sdarfsdaf

--- bot/risk/levels_calculator.py
+++
@@ -1,6 +0,0 @@
-def calculate_levels(clusters, atr, direction):
-    entry = clusters['price_mode']
-    tp1 = entry + (atr * 1.5) if direction == 'long' else entry - (atr * 1.5)
-    tp2 = entry + (atr * 3) if direction == 'long' else entry - (atr * 3)
-    stop = entry - (atr * 1) if direction == 'long' else entry + (atr * 1)
-    return entry, stop, tp1, tp2
